fdb03c35576505a0fcf8d9c354febb748e159daf
sprtho <sprtho@naver.com>
2023-01-16 15:30:52 +0900

    Component Scan 관련 추가

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,13 +1,22 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan( // Appconfig Configuration 제외를 위해
+        basePackages = "hello.core", // 제외할 경우, Default로 @ComponentScan이 붙은 설정 정보 클래스의 패키지가 시작 위치
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
     
+// 컴포넌트 스캔 중복 발생    
+//    @Bean(name = "memoryMemberRepository")
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 }
