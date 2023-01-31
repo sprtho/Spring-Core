599a452eb69c79074367ab8b3ef988ae19eb2d9e
sprtho <sprtho@naver.com>
2023-01-31 14:21:43 +0900

    Lombok 기능 추가

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,22 +6,25 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor // final이 붙은 필드를 모아서 생성자를 자동으로 만들어주는 lombok 기능
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository;
     //private final Discountpolicy discountpolicy = new FixDiscountPolicy();
     //private final Discountpolicy discountpolicy = new RateDiscountPolicy();
+    private final MemberRepository memberRepository;
     private final Discountpolicy discountpolicy;
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, Discountpolicy discountpolicy) {
-        this.memberRepository = memberRepository;
-        this.discountpolicy = discountpolicy;
-    }
+//    lombok 자동 생성자(final 필드 존재 시) 기능 생성을 위한 주석 처리
+//    @Autowired // 생성자가 한개라면 Autowired 생략 가능
+//    public OrderServiceImpl(MemberRepository memberRepository, Discountpolicy discountpolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountpolicy = discountpolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
