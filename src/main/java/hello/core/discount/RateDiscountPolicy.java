31096940bf8029f6f374f6696625c14fc19abb80
sprtho <sprtho@naver.com>
2023-02-08 13:11:35 +0900

    Component Primary

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,9 +2,11 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary  //Primary Annotation 사용 시, 조회 빈이 2개여도 해당 컴포넌트에 우선순위 부여
 public class RateDiscountPolicy implements Discountpolicy {
 
     private int discountPercent = 10;
